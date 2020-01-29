e04d6cb09a8c83b3011936e96b3d8344ade118ec
nmeisburger0 <ncm5@rice.edu>
2020-01-29 00:46:51 -0600

    kdd12 config

--- new-D-FLASH/src/Config.h
+++ new-D-FLASH/src/Config.h
@@ -1,4 +1,4 @@
-#define WEBSPAM
+// #define WEBSPAM
 
 #ifdef WEBSPAM
 
@@ -21,5 +21,29 @@
 #define CMS_BUCKET_SIZE 1024
 
 #define BASEFILE "../../../dataset/webspam/webspam_trigram.svm"
+
+#endif
+
+#define KDD12
+
+#ifdef KDD12
+
+#define NUM_HASHES 8
+#define RANGE_POW 18
+
+#define NUM_TABLES 24
+#define RESERVOIR_SIZE 256
+
+#define DIMENSION 15
+#define FULL_DIMENSION 54686452
+#define NUM_DATA_VECTORS 140000000
+#define NUM_QUERY_VECTORS 10000
+#define TOPK 128
+#define AVAILABLE_TOPK 128
+
+#define CMS_HASHES 4
+#define CMS_BUCKET_SIZE 2048
+
+#define BASEFILE "../../../dataset/kdd12/kdd12"
 
 #endif
