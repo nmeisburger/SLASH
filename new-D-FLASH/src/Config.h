4749c942d9752335f025b6f5dff742ce415d440d
nmeisburger0 <ncm5@rice.edu>
2020-01-30 17:34:43 -0600

    testing

--- new-D-FLASH/src/Config.h
+++ new-D-FLASH/src/Config.h
@@ -40,11 +40,11 @@
 // #define NUM_QUERY_VECTORS 10000
 #define NUM_DATA_VECTORS 1000
 #define NUM_QUERY_VECTORS 10
-#define TOPK 128
+#define TOPK 8
 #define AVAILABLE_TOPK 128
 
 #define CMS_HASHES 4
-#define CMS_BUCKET_SIZE 2048
+#define CMS_BUCKET_SIZE 64
 
 #define BASEFILE "../../../dataset/kdd12/kdd12"
 
